32b7bd6bba89ca196af15337c1b5a7a8c71e6443
JEONG YONG GYU <wjddydrbzxzx@naver.com>
2022-05-31 23:08:52 +0900

    Modify WEB naverMAP
    Modify WEB naverMAP

--- WEB/src/java/common/util/NaverMap.java
+++ WEB/src/java/common/util/NaverMap.java
@@ -1,59 +1,61 @@
 package common.util;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.json.simple.JSONObject;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import org.springframework.stereotype.Component;
 import common.collection.ABox;
 
 @Component
 public class NaverMap {
-	//private String ncloudUrl = "https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?";	
-	private String ncloudUrl = "https://naveropenapi.apigw-pub.fin-ntruss.com/map-direction/v1/driving?";
+	// private String ncloudUrl =
+	// "https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?";
+	private String ncloudUrl = "https://naveropenapi.apigw-pub.fin-ntruss.com/map-direction/v1/driving";
 	private String clientID = "z61zcmt5wp";
-	private String clientSecretKey = "bdfimj2GgICqHHEcNJoUpLkAm58Zu6zJn7XJpROF";
-	
-	@SuppressWarnings("unchecked")
-	public String sendNaverMap(ABox param) {	
+	private String clientSecretKey = "O89vy2cYe04XasNZssFfJspMZqfUzW7qaZ9MwYch";
+
+	public String sendNaverMap(ABox param) throws IOException {
 		String result = "";
-		CloseableHttpClient httpClient = null;
-		
+		HttpURLConnection con = null;
 		try {
 			// http client 생성
-			httpClient = HttpClients.createDefault();
 
-			ncloudUrl += "start="+param.getString("start");
-			ncloudUrl += "&goal="+param.getString("goal");
-			if(param.containsKey("waypoints")) {
-				ncloudUrl += "&waypoints="+param.getString("waypoints");
+			ncloudUrl += "?start=" + param.getString("start");
+			ncloudUrl += "&goal=" + param.getString("goal");
+			if (param.containsKey("waypoints")) {
+				ncloudUrl += "&waypoints=" + param.getString("waypoints");
 			}
-			if(param.containsKey("option")) {
-				ncloudUrl += "&option="+param.getString("option");
+			if (param.containsKey("option")) {
+				ncloudUrl += "&option=" + param.getString("option");
 			}
-			HttpGet httpGet = new HttpGet(ncloudUrl);
+			ncloudUrl = ncloudUrl.replaceAll(" ", "");
+			// ncloudUrl = URLEncoder.encode(ncloudUrl, "UTF-8");
 
-			// agent 정보 설정
-			httpGet.addHeader("X-NCP-APIGW-API-KEY-ID", clientID);
-			httpGet.addHeader("X-NCP-APIGW-API-KEY", clientSecretKey);
+			URL url = new URL(ncloudUrl);
+			con = (HttpURLConnection) url.openConnection();
 
-			CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
-			String json = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
-			result = json;
-			
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		} finally {
-			try {
-				httpClient.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientID);
+			con.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecretKey);
+			int responseCode = con.getResponseCode();
+			BufferedReader br;
+			if (responseCode == 200) {
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else {
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
 			}
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			while ((inputLine = br.readLine()) != null) {
+				response.append(inputLine);
+			}
+			result = response.toString();
+			br.close();
+		} catch (Exception e) {
+			System.out.println(e);
 		}
 
 		return result;
--- WEB/src/java/shadow/user/controller/UserController.java
+++ WEB/src/java/shadow/user/controller/UserController.java
@@ -1,24 +1,12 @@
 package shadow.user.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-
 import common.collection.ABox;
 import common.collection.ABoxList;
 import common.util.NaverMap;
@@ -35,19 +23,18 @@ public class UserController {
 
 	@RequestMapping(value = "/user/test", method = RequestMethod.GET, produces = "application/json; charset=utf8")
 	public ResponseEntity<String> test() throws Exception{
-		ABox resultBox = new ABox();
-
+		String result = "";
 		try{
 			ABox jsonBox = new ABox();
 			String json="{\"goal\":\"37.31689239364219,126.84627011464801,name=\\\"안산종합여객자동차터미널\\\"\",\"start\":\"37.3627151749158,126.73882379220626,name=\\\"오이도역 수인분당선\\\"\",\"waypoints\":\"37.350593833,126.7582752593,name=생금집:37.319893151,126.8229310131,name=안산패션일번가:37.3204546056,126.8541872996,name=노적봉 공원\"}";
 			jsonBox.setJson(json);
-			resultBox.setJson(naverMap.sendNaverMap(jsonBox));
+			result = naverMap.sendNaverMap(jsonBox);
 
 		}catch(Exception e) {
 			e.printStackTrace();
-			return new ResponseEntity<String>(resultBox.aBoxToJsonObject().toString(), HttpStatus.SERVICE_UNAVAILABLE);
+			return new ResponseEntity<String>(result, HttpStatus.SERVICE_UNAVAILABLE);
 		}
-		return new ResponseEntity<String>(resultBox.aBoxToJsonObject().toString(), HttpStatus.OK);
+		return new ResponseEntity<String>(result, HttpStatus.OK);
 	}
 		
 	@RequestMapping(value = "/insert/locate", method = RequestMethod.POST, headers = "Content-Type=application/json;utf-8")
